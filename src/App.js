890e4b6d4803916f982a22f2821e03cef7d3d953
Salohy <najorosalohy1@gmail.com>
2024-10-22 21:20:28 +0200

    Ngrok modifié 2

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const Auth = () => {
     const handleRegister = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('https://2f88-41-77-17-174.ngrok-free.app/register/', {
+            const response = await axios.post('https://f412-41-77-17-174.ngrok-free.app/register/', {
                 username: registerUsername,
                 password: registerPassword,
                 email: registerEmail,
@@ -37,7 +37,7 @@ const Auth = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('https://2f88-41-77-17-174.ngrok-free.app/login/', {
+            const response = await axios.post('https://f412-41-77-17-174.ngrok-free.app/login/', {
                 username: loginUsername,
                 password: loginPassword,
             });
